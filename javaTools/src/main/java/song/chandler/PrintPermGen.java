eb4dcc1aae46ceb00d6dcd7794e8db9d1029ddd8
chandler song <chandler605@gmail.com>
2013-02-08 13:31:02 +0800

    javaTools:fix permgen object bugs.

--- javaTools/src/main/java/song/chandler/PrintPermGen.java
+++ javaTools/src/main/java/song/chandler/PrintPermGen.java
@@ -73,7 +73,7 @@ public class PrintPermGen extends Tool {
                 args = new String[] {
                     "2460"
                 };
-                throw new Exception();
+                // throw new Exception();
             } catch (Exception e) {
                 System.err
                         .println("Usage: java com.blogspot.sahyog.PrintStringTable <PID of the JVM whose string table you want to print>");
@@ -227,7 +227,7 @@ class ObjectPrinter implements SystemDictionary.ClassAndLoaderVisitor {
         }
 
         try {
-            FileUtils.write(detailFile, k.getName().asString() + "," + loaderName, true);
+            FileUtils.write(detailFile, k.getName().asString() + "," + loaderName + PrintPermGen.LineSperator, true);
         } catch (IOException e) {
             e.printStackTrace();
         }
