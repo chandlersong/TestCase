43b8b9bdb5b3d058cc540983aa4c1ce9bbe1268f
chandler song <chandler605@gmail.com>
2013-02-23 11:21:05 +0800

    TestCase:add some example

---
+++ TestCase/src/test/java/com/hilatest/common/CharTest.java
@@ -0,0 +1,13 @@
+package com.hilatest.common;
+
+import org.junit.Test;
+
+public class CharTest {
+
+    @Test
+    public void testUpperCase() {
+        char c = 'l';
+
+        System.out.println((char) (c - 32));
+    }
+}
