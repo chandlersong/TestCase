e6cb16266df8d6a649e47e410c384127aba7d840
chandler song <chandler605@gmail.com>
2013-03-20 15:47:19 +0800

    HibernateExample:add one Inheritance Join example

---
+++ HibernateExample/src/main/java/com/hilatest/hibernate/inaction/chapter5/inheritance/join/RandomStringMessage.java
@@ -0,0 +1,41 @@
+
+package com.hilatest.hibernate.inaction.chapter5.inheritance.join;
+
+public class RandomStringMessage extends Message {
+
+    private String randomString;
+
+    public String getRandomString() {
+        return randomString;
+    }
+
+    public void setRandomString(String randomString) {
+        this.randomString = randomString;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((randomString == null) ? 0 : randomString.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RandomStringMessage other = (RandomStringMessage)obj;
+        if (randomString == null) {
+            if (other.randomString != null)
+                return false;
+        } else if (!randomString.equals(other.randomString))
+            return false;
+        return true;
+    }
+
+}
