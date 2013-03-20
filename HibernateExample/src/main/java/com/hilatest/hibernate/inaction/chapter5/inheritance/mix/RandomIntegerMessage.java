3b56202c4dd78937520a8cd23127eee8f3cb2d40
chandler song <chandler605@gmail.com>
2013-03-20 16:07:14 +0800

    Hibernate Example:add one inheritance Mix example

---
+++ HibernateExample/src/main/java/com/hilatest/hibernate/inaction/chapter5/inheritance/mix/RandomIntegerMessage.java
@@ -0,0 +1,38 @@
+
+package com.hilatest.hibernate.inaction.chapter5.inheritance.mix;
+
+public class RandomIntegerMessage extends Message {
+
+    private int RandomInteger;
+
+    public int getRandomInteger() {
+        return RandomInteger;
+    }
+
+    public void setRandomInteger(int randomInteger) {
+        RandomInteger = randomInteger;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + RandomInteger;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RandomIntegerMessage other = (RandomIntegerMessage)obj;
+        if (RandomInteger != other.RandomInteger)
+            return false;
+        return true;
+    }
+
+}
