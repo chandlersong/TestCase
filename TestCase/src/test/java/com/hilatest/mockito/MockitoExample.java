73e1d22420a440294e751f0dc8550bbb5dd4316d
chandler song <chandler605@gmail.com>
2013-05-01 21:52:55 +0800

    add Mockito Example

---
+++ TestCase/src/test/java/com/hilatest/mockito/MockitoExample.java
@@ -0,0 +1,32 @@
+package com.hilatest.mockito;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class MockitoExample {
+
+    @Test
+    public void testVerify() {
+        // mock creation
+
+        @SuppressWarnings("unchecked")
+        List<String> mockedList = Mockito.mock(List.class);
+
+        // using mock object
+        /*
+         * 注释这句之后。 Mockito.verify(mockedList).add("one");会报错。
+         * 这个用处不错
+         */
+        mockedList.add("one");
+
+        mockedList.clear();
+
+        // verification
+        Mockito.verify(mockedList).add("one");
+
+        Mockito.verify(mockedList).clear();
+
+    }
+}
