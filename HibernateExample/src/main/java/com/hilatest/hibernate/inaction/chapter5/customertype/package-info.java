db1d6cc05eac620bc80e4cc9406e784371981ba0
chandler song <chandler605@gmail.com>
2013-05-17 16:57:15 +0800

    HibernateExample:update package-info in customer type

---
+++ HibernateExample/src/main/java/com/hilatest/hibernate/inaction/chapter5/customertype/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 1）nullSafeGet and  nullSafeSet is used to assemble the object from db in hibernate
+ * 
+ * usertype:
+ *   one object correspond one field in db
+ * compositeusertype:
+ *   one object can correspond multi field in db
+ */
+/**
+ * 
+ * @author chandler.song
+ * 
+ */
+
+package com.hilatest.hibernate.inaction.chapter5.customertype;
