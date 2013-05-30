79dae06b9774acba9e61340f26323aaf77329b54
chandler.song <chandler.song@cnshwb81s2w1.corp.carestreamhealth.com>
2013-05-30 17:09:36 +0800

    HibernateExample: add sorted example

---
+++ HibernateExample/src/main/java/com/hilatest/hibernate/inaction/chapter6/collection/sortedandorder/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * A sorted collectionis
+ * sorted in memory using a Java comparator. An ordered collectionis ordered at the
+ * database level using an SQLquery with an order by clause. 
+ */
+/**
+ * @author chandler.song
+ * 
+ */
+
+package com.hilatest.hibernate.inaction.chapter6.collection.sortedandorder;
