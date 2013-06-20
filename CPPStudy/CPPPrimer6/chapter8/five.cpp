29405dad5185e2aef015dd38697bc8d0b1ce8249
chandler.song <chandler605@gmail.com>
2013-06-20 10:45:59 +0800

    cppstudy: exercise for chapter8

---
+++ CPPStudy/CPPPrimer6/chapter8/five.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+void max5(T* array);
+
+
+int main(){
+
+    int int_array[5] = {1,2,3,4,5};
+    cout << "int array" << endl;
+    max5(int_array);
+    
+    double double_array[5] = {1.0,2.0,3.0,4.0,5.0};
+    cout << "double array" << endl;
+    max5(double_array);
+    
+    
+    return 0;
+}
+
+template <typename T>
+void max5(T* array){
+     T max;
+     max = array[0];
+     for(int i = 0; i < 5 ; i ++){
+        if(max < array[i]){
+            max = array[i];
+        }
+     }
+     
+     cout << "max five is : " << max << endl;
+}
