81b5f90f0e83d587b626387c4d7b44338d6d764b
19010745 <qingxin.song@carestream.com>
2014-01-28 15:33:03 +0800

    rename cppTestCase to cmakestudy and finish step 2 phrase 1

---
+++ CMakeStudy/CMakeStudy.cxx
@@ -0,0 +1,20 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "CMakeStudy.h"
+#include "MathFunctions/MathFunctions.h"
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stdout, "Usage: %s number\n", argv[0]);
+    return 1;
+  }
+
+  double inputValue = 1;
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout, "The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
