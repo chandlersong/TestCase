cc3d5a3e703ecacd9203a662dd727d704ea6e746
19010745 <qingxin.song@carestream.com>
2014-03-06 13:17:29 +0800

    add example how to define in cpp

---
+++ CPPTestCase/StdStudy/DefineTest.h
@@ -0,0 +1,27 @@
+#ifndef CPPTESTCASE_STDSTUDY_DEFINETEST_H_
+#define CPPTESTCASE_STDSTUDY_DEFINETEST_H_
+
+
+
+namespace cpp_test_case_stdstudy {
+
+class Example;
+class DefineTest {
+
+ public:
+  /*
+  * 如果在定义函数的这里。exmaple有参数。就会涉及一些莫名其妙的问题。
+  * 比如说，Example有一个string的参数，编译能够通过。但是在使用时，传入string，就会
+  * 自动转换成Example.这样会有问题。
+  */
+  void testDefine(const Example& foo);
+ private:
+  Example* ref;
+  Example& point;
+
+  static Example a;
+};
+}
+
+
+#endif
---
+++ CPPTestCase/StdStudy/Example.h
@@ -0,0 +1,15 @@
+#ifndef CPPTESTCASE_STDSTUDY_EXAMPLE_H_
+#define CPPTESTCASE_STDSTUDY_EXAMPLE_H_
+
+#include <string>
+
+namespace cpp_test_case_stdstudy {
+
+class Example {
+
+};
+
+}
+
+
+#endif
