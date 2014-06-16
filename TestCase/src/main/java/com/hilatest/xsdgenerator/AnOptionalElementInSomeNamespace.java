f1205ec7fc201c0883f7c4665500228540675181
chandler.song <chandler605@gmail.com>
2014-06-16 16:53:10 +0800

    Use JIbx to generate code

---
+++ TestCase/src/main/java/com/hilatest/xsdgenerator/AnOptionalElementInSomeNamespace.java
@@ -0,0 +1,32 @@
+
+package com.hilatest.xsdgenerator;
+
+/** 
+ * Schema fragment(s) for this class:
+ * <pre>
+ * &lt;xs:element xmlns:ns="http://some/namespace" xmlns:xs="http://www.w3.org/2001/XMLSchema" type="xs:string" name="anOptionalElementInSomeNamespace"/>
+ * </pre>
+ */
+public class AnOptionalElementInSomeNamespace
+{
+    private String anOptionalElementInSomeNamespace;
+
+    /** 
+     * Get the 'anOptionalElementInSomeNamespace' element value.
+     * 
+     * @return value
+     */
+    public String getAnOptionalElementInSomeNamespace() {
+        return anOptionalElementInSomeNamespace;
+    }
+
+    /** 
+     * Set the 'anOptionalElementInSomeNamespace' element value.
+     * 
+     * @param anOptionalElementInSomeNamespace
+     */
+    public void setAnOptionalElementInSomeNamespace(
+            String anOptionalElementInSomeNamespace) {
+        this.anOptionalElementInSomeNamespace = anOptionalElementInSomeNamespace;
+    }
+}
--- TestCase/src/main/java/com/hilatest/xsdgenerator/FooBar.java
+++ TestCase/src/main/java/com/hilatest/xsdgenerator/FooBar.java
@@ -1,151 +1,95 @@
-//
-// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.7 生成的
-// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2014.06.16 时间 03:54:50 PM CST 
-//
-
 
 package com.hilatest.xsdgenerator;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
-
-
-/**
- * <p>fooBar complex type的 Java 类。
- * 
- * <p>以下模式片段指定包含在此类中的预期内容。
- * 
+/** 
+ * Schema fragment(s) for this class:
  * <pre>
- * &lt;complexType name="fooBar">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="aa" type="{http://www.w3.org/2001/XMLSchema}string"/>
- *         &lt;element name="optionalElement" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *       &lt;/sequence>
- *       &lt;attribute name="requiredAttribute" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
- *       &lt;attribute name="optionalAttribute" type="{http://www.w3.org/2001/XMLSchema}string" />
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
+ * &lt;xs:complexType xmlns:ns="http://some/namespace" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="fooBar">
+ *   &lt;xs:sequence>
+ *     &lt;xs:element type="xs:string" default="requiredElementValue" name="requiredElement"/>
+ *     &lt;xs:element type="xs:string" name="optionalElement" minOccurs="0"/>
+ *   &lt;/xs:sequence>
+ *   &lt;xs:attribute type="xs:string" use="required" name="requiredAttribute"/>
+ *   &lt;xs:attribute type="xs:string" name="optionalAttribute"/>
+ * &lt;/xs:complexType>
  * </pre>
- * 
- * 
  */
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "fooBar", propOrder = {
-    "aa",
-    "optionalElement"
-})
-public class FooBar {
+public class FooBar
+{
+    private String requiredElement;
+    private String optionalElement;
+    private String requiredAttribute;
+    private String optionalAttribute;
 
-    @XmlElement(required = true, defaultValue = "requiredElementValue")
-    protected String aa;
-    protected String optionalElement;
-    @XmlAttribute(name = "requiredAttribute", required = true)
-    protected String requiredAttribute;
-    @XmlAttribute(name = "optionalAttribute")
-    protected String optionalAttribute;
-
-    /**
-     * 获取aa属性的值。
+    /** 
+     * Get the 'requiredElement' element value.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
+     * @return value
      */
-    public String getAa() {
-        return aa;
+    public String getRequiredElement() {
+        return requiredElement;
     }
 
-    /**
-     * 设置aa属性的值。
+    /** 
+     * Set the 'requiredElement' element value.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
+     * @param requiredElement
      */
-    public void setAa(String value) {
-        this.aa = value;
+    public void setRequiredElement(String requiredElement) {
+        this.requiredElement = requiredElement;
     }
 
-    /**
-     * 获取optionalElement属性的值。
+    /** 
+     * Get the 'optionalElement' element value.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
+     * @return value
      */
     public String getOptionalElement() {
         return optionalElement;
     }
 
-    /**
-     * 设置optionalElement属性的值。
+    /** 
+     * Set the 'optionalElement' element value.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
+     * @param optionalElement
      */
-    public void setOptionalElement(String value) {
-        this.optionalElement = value;
+    public void setOptionalElement(String optionalElement) {
+        this.optionalElement = optionalElement;
     }
 
-    /**
-     * 获取requiredAttribute属性的值。
+    /** 
+     * Get the 'requiredAttribute' attribute value.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
+     * @return value
      */
     public String getRequiredAttribute() {
         return requiredAttribute;
     }
 
-    /**
-     * 设置requiredAttribute属性的值。
+    /** 
+     * Set the 'requiredAttribute' attribute value.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
+     * @param requiredAttribute
      */
-    public void setRequiredAttribute(String value) {
-        this.requiredAttribute = value;
+    public void setRequiredAttribute(String requiredAttribute) {
+        this.requiredAttribute = requiredAttribute;
     }
 
-    /**
-     * 获取optionalAttribute属性的值。
+    /** 
+     * Get the 'optionalAttribute' attribute value.
      * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
+     * @return value
      */
     public String getOptionalAttribute() {
         return optionalAttribute;
     }
 
-    /**
-     * 设置optionalAttribute属性的值。
+    /** 
+     * Set the 'optionalAttribute' attribute value.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
+     * @param optionalAttribute
      */
-    public void setOptionalAttribute(String value) {
-        this.optionalAttribute = value;
+    public void setOptionalAttribute(String optionalAttribute) {
+        this.optionalAttribute = optionalAttribute;
     }
-
 }
