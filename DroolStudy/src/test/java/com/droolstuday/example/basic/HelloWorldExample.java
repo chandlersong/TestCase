f6a100a7db739ddab1d8ae014964e4f78ed545a1
chandlersong <chandler605@gmail.com>
2014-08-14 17:27:49 +0800

    add spring integration of drool

--- DroolStudy/src/test/java/com/droolstuday/example/basic/HelloWorldExample.java
+++ DroolStudy/src/test/java/com/droolstuday/example/basic/HelloWorldExample.java
@@ -1,12 +1,20 @@
 package com.droolstuday.example.basic;
 
+import javax.annotation.Resource;
+
 import org.drools.runtime.StatelessKnowledgeSession;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.droolstuday.example.model.Account;
 import com.droolstuday.example.utils.DroolUtils;
 
-public class HelloWorldExample {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:DroolSessionAll.xml")
+public class HelloWorldExample extends AbstractJUnit4SpringContextTests {
 
 	@Test
 	public void helloWorld() {
@@ -21,6 +29,17 @@ public class HelloWorldExample {
 
 		Object object = new Object();
 		ksession.execute(object);
+
+	}
+
+	@Resource(name = "ksessionAllStateless")
+	private StatelessKnowledgeSession ksessionAll;
+
+	@Test
+	public void helloWorldWithSpring() {
+		Account account1 = new Account(200);
+		account1.withdraw(150);
+		ksessionAll.execute(account1);
 
 	}
 
