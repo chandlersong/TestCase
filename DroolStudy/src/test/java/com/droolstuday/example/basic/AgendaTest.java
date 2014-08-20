d6cf92e129b1691811346210763e0ca4f3439dd1
chandlersong <chandler605@gmail.com>
2014-08-20 14:35:27 +0800

    add example of Drool Agenda's Group

--- DroolStudy/src/test/java/com/droolstuday/example/basic/AgendaTest.java
+++ DroolStudy/src/test/java/com/droolstuday/example/basic/AgendaTest.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kie.api.runtime.rule.Agenda;
 import org.kie.internal.runtime.StatefulKnowledgeSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
@@ -22,6 +23,20 @@ public class AgendaTest extends AbstractJUnit4SpringContextTests {
         ksession.insert(fact);
 
         ksession.fireAllRules();
+    }
+
+    @Test
+    public void testGroup() {
+        AgendaObject fact = new AgendaObject("group");
+
+        ksession.insert(fact);
+
+        Agenda agenda = ksession.getAgenda();
+        agenda.getAgendaGroup("group1").setFocus();
+        agenda.getAgendaGroup("group2").setFocus();
+        agenda.getAgendaGroup("group3").setFocus();
+        ksession.fireAllRules();
+
     }
 
     @Resource(name = "ksessionAllStateful")
