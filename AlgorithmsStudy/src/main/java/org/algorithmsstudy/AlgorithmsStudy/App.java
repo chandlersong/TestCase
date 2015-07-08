933df4b78a2ffebe4bef704c45c4fe123011514c
chandlersong <chandler605@gmail.com>
2015-07-08 17:08:01 +0800

    add AlgorithmsStudy project

---
+++ AlgorithmsStudy/src/main/java/org/algorithmsstudy/AlgorithmsStudy/App.java
@@ -0,0 +1,13 @@
+package org.algorithmsstudy.AlgorithmsStudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
