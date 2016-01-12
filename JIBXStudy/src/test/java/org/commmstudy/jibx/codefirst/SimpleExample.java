c689f51313ca83cf58c36f8264d54b180ca7b5d7
chandlersong <chandler605@gmail.com>
2016-01-12 11:41:10 +0800

    update testcase

--- JIBXStudy/src/test/java/org/commmstudy/jibx/codefirst/SimpleExample.java
+++ JIBXStudy/src/test/java/org/commmstudy/jibx/codefirst/SimpleExample.java
@@ -1,9 +1,13 @@
 package org.commmstudy.jibx.codefirst;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import org.commmstudy.jibx.codefirst.entity.Person;
 import org.commmstudy.jibx.utility.JibxTransformer;
+import org.jibx.runtime.BindingDirectory;
+import org.jibx.runtime.IBindingFactory;
+import org.jibx.runtime.IUnmarshallingContext;
 import org.jibx.runtime.JiBXException;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +28,15 @@ public class SimpleExample {
         System.out.println(person);
     }
 
+    @Test
+    public void testTwoUnMarshall() throws FileNotFoundException, JiBXException {
+        IBindingFactory bfact = BindingDirectory.getFactory(Person.class);
+        IUnmarshallingContext uctx = bfact.createUnmarshallingContext();
+        FileInputStream in = new FileInputStream(TWO_PERSON_FILE);
+        uctx.setDocument(in, "UTF-8");
+        System.out.println(uctx.getStackDepth());
+    }
+
     private JibxTransformer<Person> trasformer;
 
     @Before
@@ -32,4 +45,6 @@ public class SimpleExample {
     }
 
     private final static String ONE_PERSON_FILE = "src/test/resources/testData/OnePerson.xml";
+
+    private final static String TWO_PERSON_FILE = "src/test/resources/testData/TwoPerson.xml";
 }
