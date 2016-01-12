32c0fe782ca3ec148213906c1726dbbed5a25507
chandlersong <chandler605@gmail.com>
2016-01-12 18:27:18 +0800

    add one example

---
+++ HibernateExample/src/main/java/org/commmstudy/hibernate/jpa/relationship/refernoid/dao/PersonDao.java
@@ -0,0 +1,9 @@
+
+package org.commmstudy.hibernate.jpa.relationship.refernoid.dao;
+
+import org.commmstudy.hibernate.jpa.relationship.refernoid.entity.Person;
+
+public interface PersonDao {
+
+	public void savePerson(Person p);
+}
