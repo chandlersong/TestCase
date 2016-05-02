2b1ce9b5adb898c939fdc8116372477e1c2cd214
chandlersong <chandler605@gmail.com>
2016-05-02 14:48:13 +0800

    rename example and add some more example

--- DataAnalysisStudy/pydata/ch7/combineData.py
+++ DataAnalysisStudy/pydata/ch7/combineData.py
@@ -64,4 +64,63 @@ another = DataFrame([[7, 8], [9, 10], [11, 12], [16, 17]],
                     columns=['New York', 'Oregon'])
 print(another)
 print(left2.join([right2, another]))
-print(left2.join([right2, another],how='outer'))
+print(left2.join([right2, another], how='outer'))
+
+arr = np.arange(12).reshape((3, 4))
+print(arr)
+print(np.concatenate([arr, arr], axis=1))
+
+s1 = Series([0, 1], index=['a', 'b'])
+s2 = Series([2, 3, 4], index=['c', 'd', 'e'])
+s3 = Series([5, 6], index=['f', 'g'])
+
+print(pd.concat([s1, s2, s3]))  # it will copy the data
+print(pd.concat([s1, s2, s3], axis=1))  # to a DataFrame
+
+s4 = pd.concat([s1 * 5, s3])
+print(s4)
+print(pd.concat([s1, s4], axis=1))
+print(pd.concat([s1, s4], axis=1, join='inner'))
+print(pd.concat([s1, s4], axis=1, join_axes=[['a', 'c', 'b', 'e']]))
+result = pd.concat([s1, s1, s3], keys=['one', 'two', 'three'])
+print(result)
+print(result.unstack())
+
+df1 = DataFrame(np.arange(6).reshape(3, 2),
+                index=['a', 'b', 'c'],
+                columns=['one', 'two'])
+df2 = DataFrame(5 + np.arange(4).reshape(2, 2),
+                index=['a', 'c'],
+                columns=['three', 'four'])
+print(pd.concat([df1, df2], axis=1, keys=['level1', 'level2']))
+print(pd.concat({'level1': df1, 'level2': df2}, axis=1))
+
+df1 = DataFrame(np.random.randn(3, 4), columns=['a', 'b', 'c', 'd'])
+df2 = DataFrame(np.random.randn(2, 3), columns=['b', 'd', 'a'])
+print(df1)
+print(df2)
+print(pd.concat([df1, df2]))
+print(pd.concat([df1, df2], ignore_index=True))
+
+a = Series([np.nan, 2.5, np.nan, 3.5, 4.5, np.nan],
+           index=['f', 'e', 'd', 'c', 'b', 'a'])
+b = Series(np.arange(len(a)),
+           dtype=np.float64,
+           index=['f', 'e', 'd', 'c', 'b', 'a'])
+b[-1] = np.nan
+
+print(a)
+print(b)
+print(np.where(pd.isnull(a), b, a))
+print(b[:-2].combine_first(a[2:]))
+
+df1 = DataFrame({'a': [1, np.nan, 5, np.nan],
+                 'b': [np.nan, 2, np.nan, 6],
+                 'c': range(2, 18, 4)
+                 })
+
+df2 = DataFrame({'a': [5, 4, np.nan, 3, 7],
+                 'b': [np.nan, 3, 4, 6, 8]
+ })
+
+print(df1.combine_first(df2))
