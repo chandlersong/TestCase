a9c19fefc780180ede15f85a502525f9f5057930
chandlersong <chandler605@gmail.com>
2016-06-02 15:52:45 +0800

    add example of how to show diagram in pycharm

--- DataAnalysisStudy/pydata/ch10/Diagram.py
+++ DataAnalysisStudy/pydata/ch10/Diagram.py
@@ -3,8 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.interactive(True)
-plt.show()
 
 
 close_px_all = pd.read_csv('../ch09/stock_px.csv', parse_dates=True, index_col=0)
@@ -15,5 +13,5 @@ close_px = close_px.resample('B').ffill()
 print(close_px)
 
 close_px['AAPL'].plot()
-
+plt.show()
 print('block')
