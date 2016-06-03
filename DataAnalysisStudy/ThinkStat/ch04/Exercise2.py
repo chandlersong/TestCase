0c88390579b0bd2285300900b640c1bac0a2fa8e
chandlersong <chandler605@gmail.com>
2016-06-03 22:47:04 +0800

    update CDF

--- DataAnalysisStudy/ThinkStat/ch04/Exercise2.py
+++ DataAnalysisStudy/ThinkStat/ch04/Exercise2.py
@@ -6,13 +6,23 @@ array =Series( [random.random() for _ in range(1000)])
 # print(array)
 
 pmf = array.value_counts()
-print(pmf)
+# print(pmf)
 
-pmf.hist(bins=50)
-plt.show()
-
-cdf = array.sort_values()
-cdf.index = range(1,len(cdf) + 1)
+# pmf.hist(bins=50)
+# plt.show()u
+temp = pmf.sort_index()
+cdf = temp.cumsum() / temp.sum()
 # print(cdf)
 cdf.plot()
 plt.show()
+
+
+def calculateCdf(series_array):
+    pmf = series_array.value_counts()
+    temp = pmf.sort_index()
+    return temp.cumsum() / temp.sum()
+
+
+test = Series([1,2,2,3,5])
+test_cdf = calculateCdf(test)
+print(test_cdf)
