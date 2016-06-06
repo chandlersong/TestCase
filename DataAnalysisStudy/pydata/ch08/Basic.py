4686eebeb7329a9d3062f633db108ba289a25a29
chandlersong <chandler605@gmail.com>
2016-06-06 11:34:07 +0800

    move example code to notebook

--- DataAnalysisStudy/pydata/ch08/Basic.py
+++ DataAnalysisStudy/pydata/ch08/Basic.py
@@ -9,5 +9,7 @@ ax3 = fig.add_subplot(2, 2, 3)
 plt.plot(randn(50).cumsum(), 'k--')
 _ = ax1.hist(randn(100), bins=20, color='k', alpha=0.3)
 ax2.scatter(np.arange(30), np.arange(30) + 3 * randn(30))
-fig.show()
-   
+plt.show()
+
+plt. plot( randn( 30). cumsum(), 'ko--')
+plt.show()
