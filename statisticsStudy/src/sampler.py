a3e677012542817d447b00290b3d3ec5a5ca42e9
chandlersong <chandler605@gmail.com>
2016-06-14 22:17:54 +0800

    add example how to calculate percent confidence interval

--- statisticsStudy/src/sampler.py
+++ statisticsStudy/src/sampler.py
@@ -1,5 +1,5 @@
 import math
-from scipy.stats import norm,t
+from scipy.stats import norm, t
 from intervals import FloatInterval
 
 
@@ -7,7 +7,7 @@ def calculate_mean_confidence_interval_large(series, confidence_interval=0.90):
     mean = series.mean()
     s = math.sqrt(series.var())
     count = series.count()
-    z = norm.isf((1-confidence_interval)/2)
+    z = norm.isf((1 - confidence_interval) / 2)
     delta = round(z * (s / math.sqrt(count)), 1)
     return FloatInterval.closed(mean - delta, mean + delta)
 
@@ -16,7 +16,13 @@ def calculate_mean_confidence_interval_small(series, confidence_interval=0.95):
     mean = series.mean()
     s = math.sqrt(series.var())
     count = series.count()
-    rv = t(count-1)
-    z = rv.isf((1-confidence_interval)/2)
+    rv = t(count - 1)
+    z = rv.isf((1 - confidence_interval) / 2)
     delta = round(z * (s / math.sqrt(count)), 2)
     return FloatInterval.closed(mean - delta, mean + delta)
+
+
+def calculate_percent_confidence_interval_large(p, n, confidence_interval=0.95):
+    z = norm.isf((1 - confidence_interval) / 2)
+    delta = round(z * math.sqrt(((p * (1 - p) )/ n)),4)
+    return FloatInterval.closed(p - delta, p + delta)
