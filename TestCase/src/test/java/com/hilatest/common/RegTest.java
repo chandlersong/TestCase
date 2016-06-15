722dfc3212cab093b2a5a6fc50076aeb434fc08e
chandlersong <chandler605@gmail.com>
2016-06-15 11:39:09 +0800

    add a reg group example

--- TestCase/src/test/java/com/hilatest/common/RegTest.java
+++ TestCase/src/test/java/com/hilatest/common/RegTest.java
@@ -1,6 +1,9 @@
 
 package com.hilatest.common;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,5 +15,17 @@ public class RegTest {
 
         Assert.assertTrue("basbfsabf".matches(regExp));
         Assert.assertTrue("http://sfbfsabfsb:12431243".matches(regExp));
+    }
+
+    @Test
+    public void testGroup() {
+        String line = "This order was placed for QT3000! OK?";
+        Pattern pattern = Pattern.compile("(.*?)(\\d+)(.*)");
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            System.out.println("group 1: " + matcher.group(1));
+            System.out.println("group 2: " + matcher.group(2));
+            System.out.println("group 3: " + matcher.group(3));
+        }
     }
 }
