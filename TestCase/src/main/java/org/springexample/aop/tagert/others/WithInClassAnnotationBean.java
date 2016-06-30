5826ddfd1bd59e364ae04ddcef88c0f95097419d
chandlersong <chandler605@gmail.com>
2016-06-30 15:11:08 +0800

    add an example of annotation aop

--- TestCase/src/main/java/org/springexample/aop/tagert/others/WithInClassAnnotationBean.java
+++ TestCase/src/main/java/org/springexample/aop/tagert/others/WithInClassAnnotationBean.java
@@ -1,12 +1,17 @@
 package org.springexample.aop.tagert.others;
 
+import org.springexample.aop.tagert.annotation.AnationWithPara;
 import org.springexample.aop.tagert.annotation.ClassExample;
 
-
 public class WithInClassAnnotationBean {
-	
-	@ClassExample
-	public void test(){
-		System.out.println(this.getClass()+"--Method:test");
-	}
+
+    @ClassExample
+    public void test() {
+        System.out.println(this.getClass() + "--Method:test");
+    }
+
+    @AnationWithPara(name = "abc")
+    public void testWithPara() {
+
+    }
 }
