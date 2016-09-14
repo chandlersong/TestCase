51241a0b65715efdeddc2c9d05b1ab96a9f702f4
chandlersong <chandler605@gmail.com>
2016-09-15 01:37:02 +0800

    update an example

--- pythonStudy/study/magic_method.py
+++ pythonStudy/study/magic_method.py
@@ -2,14 +2,16 @@ from unittest import TestCase
 import unittest
 
 class MagicExample(TestCase):
-    def test_method(self):
+    def test_getattr(self):
         class C:
             def __getattr__(self, item):
-                def wrap(*args):
+                def wrap(*args,**kwargs):
                     print("I am wrap")
                     print(args)
-                print(item)
+                    print(kwargs.get('a'))
+                print(type(item))
                 return wrap
-            
+
         c = C()
+        c.not_exist(1,a=True)
         c.not_exist(1)
