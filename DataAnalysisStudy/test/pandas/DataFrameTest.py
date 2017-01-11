ad40ed77a15f71e729ee51ddc0b112968dc3b0a3
chandlersong <chandler605@gmail.com>
2017-01-11 16:55:10 +0800

    pandas.dataFrame:an example of how to change index to column and change back

--- DataAnalysisStudy/test/pandas/DataFrameTest.py
+++ DataAnalysisStudy/test/pandas/DataFrameTest.py
@@ -6,10 +6,8 @@ from pandas import DataFrame
 
 
 class TestDataFrameExample(TestCase):
-
     def setUp(self):
-        self.example = DataFrame(randn(5,2),index=range(0,10,2),columns=list('AB'))
-
+        self.example = DataFrame(randn(5, 2), index=range(0, 10, 2), columns=list('AB'))
 
     def test_extract_new_class(self):
         print(self.example)
@@ -18,6 +16,23 @@ class TestDataFrameExample(TestCase):
         # the index which is 2
         print(self.example.loc[[2]])
 
+    def test_change_index_column(self):
+        print("print origin")
+        print(self.example)
+        print(self.example.index)
+        # index to columns
+        # self.example['index1'] = self.example.index
+        self.example.reset_index(level=0, inplace=True)
+        print("print index to column")
+        print(self.example)
+        print(self.example.index)
+        # columns to index
+        print("print column to index")
+        print(self.example.set_index(['index']))
+        print(self.example.set_index(['index'], append=True))
+        self.example.set_index(['index'], inplace=True)
+        print(self.example)
+        print(self.example.index)
 
 
 if __name__ == '__main__':
