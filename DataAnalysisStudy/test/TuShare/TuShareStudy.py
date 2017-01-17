7fcdd56973421aa01cbf18406a114e23274cd80e
chandlersong <chandler605@gmail.com>
2017-01-17 16:47:26 +0800

    TuShareStudy.TestStockPrices:add some example

--- DataAnalysisStudy/test/TuShare/TuShareStudy.py
+++ DataAnalysisStudy/test/TuShare/TuShareStudy.py
@@ -22,5 +22,15 @@ class TestFundamentalDataInfo(TestCase):
         df.to_json("new_index_values.json", orient="values")
 
 
+class TestStockPrices(TestCase):
+    def test_stock_prices(self):
+        df = ts.get_h_data('600000', start='2015-01-01', end='2016-03-16')
+        df.to_json("stock_prices_index.json", orient="index")
+        df.to_json("stock_prices_split.json", orient="split")
+        df.to_json("stock_prices_records.json", orient="index")
+        df.to_json("stock_prices_columns.json", orient="columns")
+        df.to_json("stock_prices_values.json", orient="values")
+
+
 if __name__ == '__main__':
     unittest.main()
