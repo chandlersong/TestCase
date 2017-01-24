1353cf0e55f1f59984cdc8cffb1b5092fb12d8af
chandlersong <chandler605@gmail.com>
2017-01-24 17:57:34 +0800

    matplotlib:add example how to add text

--- DataAnalysisStudy/test/matplotlib/MultipleFiguresAndAxes.py
+++ DataAnalysisStudy/test/matplotlib/MultipleFiguresAndAxes.py
@@ -36,6 +36,24 @@ class HelloWorldTest(TestCase):
         plt.title('Easy as 1, 2, 3')  # subplot 211 title
         plt.show()
 
+    def test_show_test(self):
+        # Fixing random state for reproducibility
+        np.random.seed(19680801)
+
+        mu, sigma = 100, 15
+        x = mu + sigma * np.random.randn(10000)
+
+        # the histogram of the data
+        n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.75)
+
+        plt.xlabel('Smarts')
+        plt.ylabel('Probability')
+        plt.title('Histogram of IQ')
+        plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+        plt.axis([40, 160, 0, 0.03])
+        plt.grid(True)
+        plt.show()
+
 
 if __name__ == '__main__':
     unittest.main()
