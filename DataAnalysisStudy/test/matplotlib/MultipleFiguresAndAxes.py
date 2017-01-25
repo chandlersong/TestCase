ae84829e5a781bf9d2a979a9a91d91439d4dde2b
chandlersong <chandler605@gmail.com>
2017-01-25 17:35:17 +0800

    matplotlib:add some example

--- DataAnalysisStudy/test/matplotlib/MultipleFiguresAndAxes.py
+++ DataAnalysisStudy/test/matplotlib/MultipleFiguresAndAxes.py
@@ -54,6 +54,46 @@ class HelloWorldTest(TestCase):
         plt.grid(True)
         plt.show()
 
+    def test_annotating_text(self):
+        ax = plt.subplot(111)
+
+        t = np.arange(0.0, 5.0, 0.01)
+        s = np.cos(2 * np.pi * t)
+        line, = plt.plot(t, s, lw=2)
+
+        plt.annotate('local max', xy=(2, 1), xytext=(3, 1.5),
+                     arrowprops=dict(facecolor='black', shrink=0.05),
+                     )
+
+        plt.ylim(-2, 2)
+        plt.show()
+
+
+class ArtistExample(TestCase):
+
+    def test_axs(self):
+        fig = plt.figure()
+        fig.subplots_adjust(top=0.8)
+        ax1 = fig.add_subplot(211)
+        ax1.set_ylabel('volts')
+        ax1.set_title('a sine wave')
+
+        t = np.arange(0.0, 1.0, 0.01)
+        s = np.sin(2 * np.pi * t)
+        line, = ax1.plot(t, s, color='blue', lw=2)
+
+        # Fixing random state for reproducibility
+        np.random.seed(19680801)
+
+        ax2 = fig.add_axes([0.15, 0.1, 0.7, 0.3])
+        n, bins, patches = ax2.hist(np.random.randn(1000), 50,
+                                    facecolor='yellow', edgecolor='yellow')
+        ax2.set_xlabel('time (s)')
+
+        axis = ax1.xaxis
+        print(axis.get_ticklocs())
+
+        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
