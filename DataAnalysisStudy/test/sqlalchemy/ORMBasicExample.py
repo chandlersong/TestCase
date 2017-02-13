b64e5d5210ecee5e5fc9a363709443db2ca0b0c0
chandlersong <chandler605@gmail.com>
2017-02-13 13:53:24 +0800

    ORMBasicExample:add an example how to save relationship data

--- DataAnalysisStudy/test/sqlalchemy/ORMBasicExample.py
+++ DataAnalysisStudy/test/sqlalchemy/ORMBasicExample.py
@@ -3,9 +3,10 @@ import unittest
 from unittest import TestCase
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 path = "config.properties"
 Base = declarative_base()
@@ -36,6 +37,14 @@ class TestBasic(TestCase):
 
         self.session.commit()
 
+    def test_relationship(self):
+        ed_user = RelationshipUser(name='ed', fullname='Ed Jones', password='edspassword')
+        address1 = RelationshipAddress(email_address="aa")
+        address2 = RelationshipAddress(email_address="bb")
+        ed_user.addresses = [address1,address2]
+
+        self.session.add(ed_user)
+        self.session.commit()
 
 class BasicUser(Base):
     __tablename__ = "basic_user"
@@ -48,6 +57,35 @@ class BasicUser(Base):
     def __repr__(self):
         return "<User(name='%s', fullname='%s', password='%s')>" % (
             self.name, self.fullname, self.password)
+
+
+class RelationshipUser(Base):
+    __tablename__ = "relationship_user"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    addresses = relationship("RelationshipAddress", back_populates='user',
+                cascade = "all, delete, delete-orphan")
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+            self.name, self.fullname, self.password)
+
+
+class RelationshipAddress(Base):
+    __tablename__ = 'relationship_addresses'
+
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('relationship_user.id'))
+
+    user = relationship("RelationshipUser", back_populates="addresses")
+
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
 
 
 if __name__ == '__main__':
