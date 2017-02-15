038aa33f1b4aa75516c6757915473f1db4ede6f3
chandlersong <chandler605@gmail.com>
2017-02-15 16:36:58 +0800

    ExpressionsExample:add some example about how to trigger event

--- DataAnalysisStudy/test/sqlalchemy/ExpressionsExample.py
+++ DataAnalysisStudy/test/sqlalchemy/ExpressionsExample.py
@@ -18,15 +18,20 @@ class TestExpressions(TestCase):
     def setUp(self):
         self.config = configparser.RawConfigParser()
         self.config.read(path)
-        self.engine = create_engine(self.config.get('DataBase', 'db_link'), echo=True)
+        self.engine = create_engine(self.config.get('DataBase', 'db_link'), echo=False)
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)()
 
     def test_hello_world(self):
         ed_user = ExpressionUser(firstname='ed', lastname='Ed Jones')
         self.session.add(ed_user)
+        # trigger before_update
+        self.session.commit()
+        # trigger before_update
         ed_user._fullname = "bb"
         self.session.commit()
+        # won't trigger update
+        ed_user.another_property = "changed"
         for instance in self.session.query(ExpressionUser):
             print(instance)
 
@@ -39,6 +44,9 @@ class ExpressionUser(Base):
 
     _fullname = Column("fullname", String)
 
+    def __init__(self, another_property="aa", *args, **kwargs):
+        self.__another_property = another_property
+
     @property
     def fullname(self):
         return "aa"
@@ -47,6 +55,14 @@ class ExpressionUser(Base):
     def fullname(self, fullname):
         print(fullname)
 
+    @property
+    def another_property(self):
+        return "aa"
+
+    @another_property.setter
+    def another_property(self, another_property):
+        print("another_property")
+
 
 """
 event refer to
@@ -54,11 +70,13 @@ event refer to
     and
     http://docs.sqlalchemy.org/en/latest/orm/session_events.html
 """
+
+
 @event.listens_for(ExpressionUser, 'before_update')
 @event.listens_for(ExpressionUser, 'before_insert')
 def set_user_fullname(apper, connection, target):
+    print('before_insert or before_update')
     print(target.fullname)
-    target._fullname = target.fullname
 
 
 @event.listens_for(ExpressionUser, 'load')
