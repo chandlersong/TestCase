064081ef0f801337e8590b218d2c6ed3356888e0
chandlersong <chandler605@gmail.com>
2017-03-07 11:44:49 +0800

    pandas.MulitIndex:example about how to create an empty dataFrame

--- DataAnalysisStudy/test/pandas/IndexTest.py
+++ DataAnalysisStudy/test/pandas/IndexTest.py
@@ -20,6 +20,27 @@ class TestMulitIndex(TestCase):
         df.loc[('five','one'),:] = [1,2,3,4]
         print(df)
 
+    def test_create_emtpy_multi_index_df(self):
+        my_index = pd.MultiIndex(levels=[[], [], []],
+                                 labels=[[], [], []],
+                                 names=[u'one', u'two', u'three'])
+        my_columns = [u'alpha', u'beta']
+
+        df = pd.DataFrame(index=my_index, columns=my_columns)
+        print("empty dataFrame")
+        print(df)
+        df.loc[('five', 'one','test'), :] = np.random.randn(2)
+        df.loc[('five', 'one', 'test2'), :] = np.random.randn(2)
+        df.loc[('five', 'two','test'), :] = np.random.randn(2)
+        df.loc[('one', 'one','test'), :] = np.random.randn(2)
+        df.loc[('one', 'two','test'), :] = np.random.randn(2)
+        df.loc[('three', 'ten','test'), :] = np.random.randn(2)
+        df.loc[('three', 'eight','test'), :] = np.random.randn(2)
+
+        print(df)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
