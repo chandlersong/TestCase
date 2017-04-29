200bf34f4e00a662bef4721c1d4220520d78bff0
chandlersong <chandler605@gmail.com>
2017-04-29 21:30:49 +0800

    HybridMethod:add an update example

--- DataAnalysisStudy/test/sqlalchemy/HybridMethod.py
+++ DataAnalysisStudy/test/sqlalchemy/HybridMethod.py
@@ -6,10 +6,12 @@ from sqlalchemy import Integer, Column, create_engine, orm
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.ext.mutable import MutableDict
 from sqlalchemy.orm import sessionmaker
 
 path = "config.properties"
 Base = declarative_base()
+
 
 class TestBasic(TestCase):
     def setUp(self):
@@ -19,35 +21,41 @@ class TestBasic(TestCase):
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)()
 
-    def test_save(self):
+    def test_add(self):
         ed_user = HyBridClass(first_name="bb")
         self.session.add(ed_user)
+        self.session.commit()
+
+    def test_update(self):
+        ed_user = HyBridClass(first_name="bb")
+        self.session.add(ed_user)
+        self.session.commit()
+        ed_user.first_name = "cc"
         self.session.commit()
 
 
 class HyBridClass(Base):
     __tablename__ = "hybrid_user"
     id = Column(Integer, primary_key=True)
-    info = Column(JSONB,default=lambda: {})
+    info = Column(MutableDict.as_mutable(JSONB))
 
-    def __init__(self,**kwargs):
-        Base.__init__(self,**kwargs)
+    def __init__(self, **kwargs):
+        if self.info is None:
+            self.info = {}
+        Base.__init__(self, **kwargs)
 
     @orm.reconstructor
     def init_on_load(self):
-      if self.info is None:
-          self.info = {}
+        if self.info is None:
+            self.info = {}
 
     @hybrid_property
     def first_name(self):
         return self.info['first_name']
 
     @first_name.setter
-    def first_name(self,first_name):
-        if self.info is None:
-            self.info = {}
+    def first_name(self, first_name):
         self.info['first_name'] = first_name
-
 
 
 if __name__ == '__main__':
