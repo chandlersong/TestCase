a578666c074126c1ea81aa1dfc77666895056fc1
chandlersong <chandler605@gmail.com>
2018-10-16 07:53:49 +0800

    add spring security for restful service

--- spring-demos/security-demos/restful-service/src/main/java/me/study/security/restful/SecurityRestfulApplication.java
+++ spring-demos/security-demos/restful-service/src/main/java/me/study/security/restful/SecurityRestfulApplication.java
@@ -2,8 +2,12 @@ package me.study.security.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@ImportResource("classpath:application-beans.xml")
+@EnableWebSecurity
 public class SecurityRestfulApplication {
 
     public static void main(String[] args) {
