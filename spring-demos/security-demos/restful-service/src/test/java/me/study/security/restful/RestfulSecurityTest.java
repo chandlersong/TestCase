770e6b4e503a893ff5c4d5e78bab3c151ee9c495
chandlersong <chandler605@gmail.com>
2018-10-22 20:48:19 +0800

    refactor the project structure

--- spring-demos/security-demos/restful-service/src/test/java/me/study/security/restful/RestfulSecurityTest.java
+++ spring-demos/security-demos/restful-service/src/test/java/me/study/security/restful/RestfulSecurityTest.java
@@ -1,63 +1,40 @@
 package me.study.security.restful;
 
-import org.apache.commons.codec.digest.Md5Crypt;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.auth.DigestScheme;
-import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
-import org.apache.tomcat.util.security.MD5Encoder;
+import me.study.spirng.tools.ServerRunner;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.codec.Hex;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class RestfulSecurityTest {
 
     private static final Logger logger = getLogger(RestfulSecurityTest.class);
 
 
-    @LocalServerPort
-    private int port;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+
 
     /**
      * 只能通过web登录，code访问懒得写了。
      */
     @Test
     public void testHttpDigest() {
+
+        ServerRunner.createAndRunServer(SecurityRestfulApplication.class, "http-digest-application.yml");
+
+
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         MessageDigest digest;
         try {
@@ -69,7 +46,7 @@ public class RestfulSecurityTest {
         }
 
 
-        ResponseEntity<String> result = restTemplate.getForEntity("/greeting", String.class);
+        ResponseEntity<String> result = (new RestTemplate()).getForEntity("http://localhost:8080/greeting", String.class);
 
 
 
---
+++ spring-demos/spring-tools/src/main/java/me/study/spirng/tools/ServerRunner.java
@@ -0,0 +1,56 @@
+package me.study.spirng.tools;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Optional;
+
+public class ServerRunner {
+
+    private static final String YML_CONFIG_PATH = "spring.config.location=classpath:%1$s";
+    private static final String BOOTSTRAP_CONFIG_PATH = "spring.cloud.bootstrap.location=classpath:%1$s";
+    private Class<?> clz;
+
+    private Optional<String> ymlPath;
+    private Optional<String> bootstrap;
+
+    private Optional<ConfigurableApplicationContext> application = Optional.empty();
+
+
+    private ServerRunner(Class<?> clz, String ymlPath, String bootstrap) {
+        this.clz = clz;
+        this.ymlPath = Optional.ofNullable(ymlPath);
+        this.bootstrap = Optional.ofNullable(bootstrap);
+    }
+
+    private ServerRunner(Class<?> clz) {
+        this(clz, null, null);
+    }
+
+    public static ServerRunner createAndRunServer(Class<?> clz, String ymlPath) {
+        return createAndRunServer(clz, ymlPath, null);
+    }
+
+    public static ServerRunner createAndRunServer(Class<?> clz, String ymlPath, String bootstrap) {
+        return (new ServerRunner(clz, ymlPath, bootstrap)).run();
+    }
+
+    public static ServerRunner createAndRunServer(Class<?> clz) {
+        return (new ServerRunner(clz)).run();
+    }
+
+    public ServerRunner run() {
+        SpringApplicationBuilder server = new SpringApplicationBuilder(clz);
+        ymlPath.ifPresent(path -> server.properties(String.format(YML_CONFIG_PATH, path)));
+
+        bootstrap.ifPresent(path -> server.properties(String.format(BOOTSTRAP_CONFIG_PATH, path)));
+        application = Optional.ofNullable(server.run());
+
+        return this;
+    }
+
+    public void stop() {
+        application.ifPresent(ConfigurableApplicationContext::close);
+    }
+}
