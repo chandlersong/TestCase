78a5984ef7ff8250547bda4afcf4177edf0f6a64
chandlersong <chandler605@gmail.com>
2019-01-04 21:56:41 +0800

    guava: add some examples of compare collections

--- guava/src/test/java/me/study/java/guava/collections/CollectionUtilsExample.java
+++ guava/src/test/java/me/study/java/guava/collections/CollectionUtilsExample.java
@@ -1,14 +1,14 @@
 package me.study.java.guava.collections;
 
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
+import com.google.common.collect.*;
 import com.google.common.primitives.Ints;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 
 import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -32,5 +32,32 @@ public class CollectionUtilsExample {
         logger.info("{} limit", Iterables.limit(concatenated, 2));
         logger.info("{} only element", Iterables.getOnlyElement(concatenated, -1)); //throw here,
 
+    }
+
+
+    @Test
+    public void testSetStaticMethod() {
+        Set<String> set1 = Sets.newHashSet("a", "b", "c", "d", "e");
+        Set<String> set2 = Sets.newHashSet("b", "c", "d", "f", "g");
+
+        logger.info("Sets. union:{}", Sets.union(set1, set2));
+        logger.info("Sets.different:{}", Sets.difference(set1, set2));
+        logger.info("Sets.intersection:{}", Sets.intersection(set1, set2));
+        logger.info("Sets.symmetricDifference:{}", Sets.symmetricDifference(set1, set2));
+        logger.info("Sets.cartesianProduct:{}", Sets.cartesianProduct(set1, set2));
+        logger.info("Sets.powerSet:{}", Sets.powerSet(set1));
+
+    }
+
+    @Test
+    public void testMapCompareMethod() {
+        Map<String, Integer> left = ImmutableMap.of("a", 1, "b", 2, "c", 3);
+        Map<String, Integer> right = ImmutableMap.of("b", 2, "c", 4, "d", 5);
+        MapDifference<String, Integer> diff = Maps.difference(left, right);
+
+        logger.info("diff.entriesInCommon:{}", diff.entriesInCommon()); // {"b" => 2}
+        logger.info("diff.entriesInCommon:{}", diff.entriesDiffering()); // {"c" => (3, 4)}
+        logger.info("diff.entriesInCommon:{}", diff.entriesOnlyOnLeft()); // {"a" => 1}
+        logger.info("diff.entriesInCommon:{}", diff.entriesOnlyOnRight()); // {"d" => 5}
     }
 }
