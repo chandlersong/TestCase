fe349fb8f6da271310a7161ab7f3553692ec51ec
chandlersong <chandler605@gmail.com>
2019-01-09 21:57:07 +0800

    lombok: add a simple example

---
+++ lombok-demo/src/test/java/me/study/java/LombokDemo.java
@@ -0,0 +1,32 @@
+package me.study.java;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+public class LombokDemo {
+
+    @Test
+    public void testGet() {
+        Entry e = new Entry();
+        e.setValue("lombok");
+        e.testHelloWorld();
+    }
+
+
+}
+
+
+@Slf4j
+class Entry {
+
+    @Setter
+    @Getter
+    private String value;
+
+    public void testHelloWorld() {
+        log.info("hello :{}", value);
+    }
+}
