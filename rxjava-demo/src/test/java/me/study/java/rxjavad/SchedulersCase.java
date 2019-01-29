ced1ef3dbbe5f8a6ade8d1afdc1c9f03fdd1bd62
chandlersong <chandler605@outlook.com>
2019-01-29 10:24:47 +0800

    rxjava:refactor the code make it clear

--- rxjava-demo/src/test/java/me/study/java/rxjavad/SchedulersCase.java
+++ rxjava-demo/src/test/java/me/study/java/rxjavad/SchedulersCase.java
@@ -1,43 +1,56 @@
 package me.study.java.rxjavad;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 public class SchedulersCase {
 
+    private Random r = new Random();
+
     @Test
     public void testSchedulesCase() throws InterruptedException {
 
         int totalNumber = 10;
-        CountDownLatch latch = new CountDownLatch(totalNumber*2);
-        Observable.create(emitter -> {
+        CountDownLatch latch = new CountDownLatch(totalNumber * 2);
+        Observable.create(SendNumber(totalNumber)).
+                observeOn(Schedulers.computation()).
+                map(printThread(latch, "step1")).
+                observeOn(Schedulers.io()).
+                map(printThread(latch, "step2")).
+                subscribe(System.out::println, Throwable::printStackTrace);
+
+        latch.await();
+    }
+
+    private Function<Object, Integer> printThread(CountDownLatch latch, String step) {
+        return x -> {
+            int i = Integer.parseInt(x.toString());
+            log.info("{} in thread {} process {}", step, Thread.currentThread().getId(), x);
+            Thread.sleep(100 + r.nextInt(100));
+            latch.countDown();
+            return i;
+        };
+    }
+
+    private ObservableOnSubscribe<Object> SendNumber(int totalNumber) {
+        return emitter -> {
             while (!emitter.isDisposed()) {
 
                 for (int i = 0; i < totalNumber; i++) {
                     emitter.onNext(i);
                     log.info("send on {} in thread {}", i, Thread.currentThread().getId());
-                    Thread.sleep(100);
+                    Thread.sleep(100 + r.nextInt(100));
                 }
                 emitter.onComplete();
             }
-        }).observeOn(Schedulers.io()).map(x -> {
-            Integer i = Integer.parseInt(x.toString());
-            log.info("first step {} in thread {}", x, Thread.currentThread().getId());
-            Thread.sleep(100);
-            latch.countDown();
-            return i * i;
-        }).observeOn(Schedulers.io()).map(i -> {
-            log.info("second step {} in thread {}", i, Thread.currentThread().getId());
-            Thread.sleep(100);
-            latch.countDown();
-            return i;
-        }).subscribe(System.out::println, Throwable::printStackTrace);
-
-        latch.await();
+        };
     }
 }
