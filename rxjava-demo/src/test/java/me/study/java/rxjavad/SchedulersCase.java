ba5b2edd96f955ebe7b3339343b692a90bb54d93
chandlersong <chandler605@gmail.com>
2019-01-29 21:47:03 +0800

    rxjava:refactor the code

--- rxjava-demo/src/test/java/me/study/java/rxjavad/SchedulersCase.java
+++ rxjava-demo/src/test/java/me/study/java/rxjavad/SchedulersCase.java
@@ -1,57 +1,48 @@
 package me.study.java.rxjavad;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 public class SchedulersCase {
 
-    private Random r = new Random();
 
+    private RxUtils rxUtils = new RxUtils();
     @Test
     public void testSchedulesCase() throws InterruptedException {
 
         int totalNumber = 10;
         CountDownLatch latch = new CountDownLatch(totalNumber * 2);
-        Observable.create(SendNumber(totalNumber)).
+        Observable.create(rxUtils.SendNumber(totalNumber)).
                 observeOn(Schedulers.computation()).
-                map(printThread(latch, "step1")).
+                map(rxUtils.printThread(latch, "step1")).
                 observeOn(Schedulers.io()).
-                map(printThread(latch, "step2")).
+                map(rxUtils.printThread(latch, "step2")).
                 subscribeOn(Schedulers.computation()).
                 subscribe(System.out::println, Throwable::printStackTrace);
 
         latch.await();
     }
 
-    private Function<Object, Integer> printThread(CountDownLatch latch, String step) {
-        return x -> {
-            int i = Integer.parseInt(x.toString());
-            log.info("{} in thread {} process {}", step, Thread.currentThread().getId(), x);
-            Thread.sleep(1000 + r.nextInt(500));
-            latch.countDown();
-            return i;
-        };
+    @Test
+    public void testOperatorConcurrent() throws InterruptedException {
+
+        int totalNumber = 10;
+        CountDownLatch latch = new CountDownLatch(totalNumber * 2);
+        Observable.create(rxUtils.SendNumber(totalNumber)).
+                observeOn(Schedulers.io()).
+                flatMap(val -> Observable.just(val)
+                        .subscribeOn(Schedulers.computation())
+                        .map(rxUtils.printThread(latch, "step1"))).
+                subscribeOn(Schedulers.computation()).
+                subscribe(System.out::println, Throwable::printStackTrace);
+
+        latch.await();
     }
 
-    private ObservableOnSubscribe<Object> SendNumber(int totalNumber) {
-        return emitter -> {
-            while (!emitter.isDisposed()) {
 
-                for (int i = 0; i < totalNumber; i++) {
-                    emitter.onNext(i);
-                    log.info("send on {} in thread {}", i, Thread.currentThread().getId());
-                    Thread.sleep(100 + r.nextInt(100));
-                }
-                emitter.onComplete();
-            }
-        };
-    }
 }
