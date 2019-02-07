05bf75160ee8710c577a89ae9e5f3e8d4f83828a
chandlersong <chandler605@gmail.com>
2019-02-07 21:22:41 +0800

    rxjava: doOnNextExamples

---
+++ rxjava-demo/src/test/java/me/study/java/rxjavad/operator/DoOnNextExamples.java
@@ -0,0 +1,28 @@
+package me.study.java.rxjavad.operator;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+import lombok.extern.slf4j.Slf4j;
+import me.study.java.rxjavad.RxUtils;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+public class DoOnNextExamples {
+
+    private RxUtils rxUtils = new RxUtils();
+
+    @Test
+    public void simpleDoOnNext() throws InterruptedException {
+        int totalNumber = 10;
+        CountDownLatch latch = new CountDownLatch(totalNumber * 2);
+        Observable.create(rxUtils.SendNumber(totalNumber)).
+                observeOn(Schedulers.io()).
+                doOnNext(i -> log.info("do on next:{}", i)).
+                subscribeOn(Schedulers.computation()).
+                subscribe(System.out::println, Throwable::printStackTrace);
+
+        latch.await();
+    }
+}
