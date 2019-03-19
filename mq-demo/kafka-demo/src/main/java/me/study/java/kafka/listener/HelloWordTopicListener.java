b80b02ea78d56b97706f101274476a843c1649a8
chandlersong <chandler605@gmail.com>
2019-03-19 21:12:09 +0800

    make the code clear

--- mq-demo/kafka-demo/src/main/java/me/study/java/kafka/listener/HelloWordTopicListener.java
+++ mq-demo/kafka-demo/src/main/java/me/study/java/kafka/listener/HelloWordTopicListener.java
@@ -3,12 +3,26 @@ package me.study.java.kafka.listener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+
 @Slf4j
 public class HelloWordTopicListener {
+
+    private CountDownLatch latch;
 
     @KafkaListener(id = "helloWorldListener", topics = "helloWorld")
     public void listen1(String name) {
         log.info("receive message:{}", name);
+        Optional.ofNullable(latch).ifPresent(CountDownLatch::countDown);
     }
 
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+    public void setLatch(CountDownLatch latch) {
+        this.latch = latch;
+    }
 }
