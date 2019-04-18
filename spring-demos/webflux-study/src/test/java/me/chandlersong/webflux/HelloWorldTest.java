9c63aacdd1692a28dc30fa61d0238b6c33b1bcd7
chandlersong <chandler605@gmail.com>
2019-04-18 20:24:37 +0800

    webflux: add simple test

---
+++ spring-demos/webflux-study/src/test/java/me/chandlersong/webflux/HelloWorldTest.java
@@ -0,0 +1,34 @@
+package me.chandlersong.webflux;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+@SpringBootTest(classes = WebFluxApplication.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
+public class HelloWorldTest {
+
+    private WebClient client = WebClient.create("http://localhost:8080");
+
+    @Test
+    public void testHelloWorld() throws InterruptedException {
+        Mono<String> helloWorld = client.get()
+                .uri("/mono/{id}", "1")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        CountDownLatch latch = new CountDownLatch(1);
+        helloWorld.subscribe(s->{
+            log.info("receive message:{}",s);
+            latch.countDown();
+        });
+        latch.await();
+    }
+}
