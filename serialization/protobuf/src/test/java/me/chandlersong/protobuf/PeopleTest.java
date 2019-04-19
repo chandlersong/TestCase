54dd15cc01dec21568245706e36d361845315477
chandlersong <chandler605@gmail.com>
2019-04-19 22:00:48 +0800

    protobuf: protobuf To json

--- serialization/protobuf/src/test/java/me/chandlersong/protobuf/PeopleTest.java
+++ serialization/protobuf/src/test/java/me/chandlersong/protobuf/PeopleTest.java
@@ -1,13 +1,19 @@
 package me.chandlersong.protobuf;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import static com.google.protobuf.util.JsonFormat.parser;
+import static me.chandlersong.protobuf.People.Person.newBuilder;
+
+@Slf4j
 public class PeopleTest {
 
     @Test
     public void testFirstProtoBuf() throws InvalidProtocolBufferException {
-        People.Person.Builder builder = People.Person.newBuilder();
+        People.Person.Builder builder = newBuilder();
         builder.setId(1);
         builder.setName("chandler");
         builder.setEmail("chandler605@gmail.com");
@@ -20,6 +26,13 @@ public class PeopleTest {
             System.out.print(b);
         }
         System.out.println("================");
+
+        String print = JsonFormat.printer().print(person);
+        log.info("json:{}", print);
+
+        People.Person.Builder jsonBuilder = newBuilder();
+        parser().merge(print, jsonBuilder);
+        log.info("person from json:{}", jsonBuilder.build());
 
         byte[] byteArray = person.toByteArray();
         People.Person p2 = People.Person.parseFrom(byteArray);
