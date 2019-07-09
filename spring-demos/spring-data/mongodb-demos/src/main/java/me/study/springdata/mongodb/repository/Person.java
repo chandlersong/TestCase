07c5bdb5048b7d51fd7e6f5ec011845dee3a4c6c
chandlersong <chandler605@gmail.com>
2019-07-09 21:14:20 +0800

    springdata.mongodb: TypeAlias function

--- spring-demos/spring-data/mongodb-demos/src/main/java/me/study/springdata/mongodb/repository/Person.java
+++ spring-demos/spring-data/mongodb-demos/src/main/java/me/study/springdata/mongodb/repository/Person.java
@@ -3,6 +3,7 @@ package me.study.springdata.mongodb.repository;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigInteger;
@@ -10,6 +11,7 @@ import java.math.BigInteger;
 @Data
 @NoArgsConstructor
 @Document(collection = "test1")
+@TypeAlias("Person")
 public class Person {
 
     @Id
