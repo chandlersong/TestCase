448f3bbeee04b38bb38a64c1691945ed47fe8161
chandlersong <chandler605@gmail.com>
2019-09-01 16:58:45 +0800

    avro: use logic type to represent timestamp

--- serialization/serialization-test/src/test/java/me/chandlersong/serialization/AvroTests.java
+++ serialization/serialization-test/src/test/java/me/chandlersong/serialization/AvroTests.java
@@ -1,8 +1,12 @@
 package me.chandlersong.serialization;
 
+import example.avro.Address;
 import example.avro.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.time.Clock;
+import java.time.Instant;
 
 @Slf4j
 public class AvroTests {
@@ -17,14 +21,21 @@ public class AvroTests {
         log.info("user:{}", user1);
         // Leave favorite color null
 
+
         // Alternate constructor
-        User user2 = new User("Ben", 7, "red");
+        User user2 = new User("Ben", 7, "red", new Address(), Instant.now(Clock.systemUTC()));
+        log.info("time stamp of user2 is {}", user2.getCreateTimestamp());
 
         // Construct via builder
         User user3 = User.newBuilder()
-                         .setName("Charlie")
-                         .setFavoriteColor("blue")
-                         .setFavoriteNumber(null)
-                         .build();
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(3)
+                .setAddress(null)
+                .setCreateTimestamp(null)
+                .build();
+
     }
+
+
 }
