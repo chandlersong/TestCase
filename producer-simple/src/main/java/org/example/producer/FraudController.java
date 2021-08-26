4dcb874a75377605719f0a0b02cc0cf1839359f9
chandlersong <chandler605@gmail.com>
2021-08-26 23:42:27 +0800

    producer: create simple example for producer

---
+++ producer-simple/src/main/java/org/example/producer/FraudController.java
@@ -0,0 +1,19 @@
+package org.example.producer;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FraudController {
+
+    @PutMapping(value = "/fraudcheck", consumes="application/json;charset=UTF-8", produces="application/json;charset=UTF-8")
+    public String check(@RequestBody LoanRequest loanRequest) {
+
+        if (loanRequest.getLoanAmount() > 10000) {
+            return "{fraudCheckStatus: FRAUD, rejection.reason: Amount too high}";
+        } else {
+            return "{fraudCheckStatus: OK, acceptance.reason: Amount OK}";
+        }
+    }
+}
